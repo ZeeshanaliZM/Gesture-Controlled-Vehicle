358f823c3f05624660c531fc02fd513793a19e85
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-26 20:53:29 +0530

    Flipped video vertically
    imshow function displays a laterally inverted image
    Flip the image to correct it

--- Gesture Controlled Vehicle/Gesture-Car-Control.py
+++ Gesture Controlled Vehicle/Gesture-Car-Control.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
     camera = cv2.VideoCapture(0)
     while True:
-        _,frame = camera.read()
+        frame = cv2.flip(camera.read()[1],1)
         cv2.imshow("Live Stream",frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
