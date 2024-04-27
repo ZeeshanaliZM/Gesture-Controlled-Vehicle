1b7299ab412ad2634a671c3720613136fd96f01e
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-27 12:12:28 +0530

    store network details
    Store ssid and password in program memory
    include ESP8266WIFI.h to allow connection to wifi

--- Vehicle-NodeMCU-Control/include/Header_definitions.h
+++ Vehicle-NodeMCU-Control/include/Header_definitions.h
@@ -1,5 +1,8 @@
 #ifndef HEADER_DEFINITIONS_H
 #define HEADER_DEFINITIONS_H
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
+const char* ssid PROGMEM = "LAPTOP-K75VLUOD 3846";
+const char* password PROGMEM = "#Su81479";
 #endif
