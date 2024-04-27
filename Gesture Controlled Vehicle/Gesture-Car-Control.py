42da95591ef8f315fda78d5010d9884b1249a15d
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-27 21:44:34 +0530

    Connecting to WebServer on ESP8266
    Client keeps trying until connection is etablished.
    Request is sent to default route.
    Sends response when connection is established.

--- Gesture Controlled Vehicle/Gesture-Car-Control.py
+++ Gesture Controlled Vehicle/Gesture-Car-Control.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import requests
 
+ESP8266_URL = "http://esp8266.local:80/"
 
 def main():
     camera = cv2.VideoCapture(0)
@@ -12,5 +14,16 @@ def main():
     camera.release()
     cv2.destroyAllWindows()
 
+def connecttoVehicle():
+    while True:
+        try:
+            response = requests.get(ESP8266_URL)
+        except requests.exceptions.ConnectionError:
+            print("Connection Error. Reconnecting...")
+            continue
+        break
+    print(response.text)
+
 if __name__ == "__main__":
+    connecttoVehicle()
     main()
