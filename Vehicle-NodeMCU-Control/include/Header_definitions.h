bcc1c827382bcbc88cd630247eecfb3e0412a11b
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-28 22:23:02 +0530

    Renamed Macros related to Enable pins to specify speed control property

--- Vehicle-NodeMCU-Control/include/Header_definitions.h
+++ Vehicle-NodeMCU-Control/include/Header_definitions.h
@@ -9,11 +9,11 @@ const char* password PROGMEM = "#Su81479";
 
 #define DRIVER_IN4_BACK D0
 #define DRIVER_IN3_BACK D1
-#define DRIVER_EN2_BACK D2
+#define DRIVER_EN2_BACK_SPEED D2
 
 #define DRIVER_IN2_FRONT D5
 #define DRIVER_IN1_FRONT D6
-#define DRIVER_EN1_FRONT D7
+#define DRIVER_EN1_FRONT_SPEED D7
 
 ESP8266WebServer server(80);
 #endif
