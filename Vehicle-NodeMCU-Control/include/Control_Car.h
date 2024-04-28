4faf4ad499645b08e8086f134e8233e92630d650
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-29 00:24:45 +0530

    COnfigure and Initialize the pins connected to the ESP8266 to control the motor

--- Vehicle-NodeMCU-Control/include/Control_Car.h
+++ Vehicle-NodeMCU-Control/include/Control_Car.h
@@ -8,3 +8,25 @@ Red Wire of Front Motor - M1
 Black Wire of Back Motor - M2
 */
 
+void setMotorPinDirection(){
+    pinMode(DRIVER_IN1_FRONT,OUTPUT);
+    pinMode(DRIVER_IN2_FRONT,OUTPUT);
+    pinMode(DRIVER_IN3_BACK,OUTPUT);
+    pinMode(DRIVER_IN4_BACK,OUTPUT);
+    pinMode(DRIVER_EN1_FRONT_DIRECTION,OUTPUT);
+    pinMode(DRIVER_EN2_BACK_SPEED,OUTPUT);
+}
+
+void initPins(){
+    digitalWrite(DRIVER_IN1_FRONT,LOW);
+    digitalWrite(DRIVER_IN2_FRONT,LOW);
+    digitalWrite(DRIVER_IN3_BACK,LOW);
+    digitalWrite(DRIVER_IN4_BACK,LOW);
+    digitalWrite(DRIVER_EN1_FRONT_DIRECTION,LOW);
+    digitalWrite(DRIVER_EN2_BACK_SPEED,LOW);
+}
+
+void configMotorPins(){
+    setMotorPinDirection();
+    initPins();
+}
