9182bc7d4d56994a45fca503bd098a4d50c0e9f8
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-29 00:20:45 +0530

    Created Control_Car.h library to control the motion of the car

---
+++ Vehicle-NodeMCU-Control/include/Control_Car.h
@@ -0,0 +1,10 @@
+#include "Header_definitions.h"
+
+/*Connections for Motor and Driver
+Red Wire of Back Motor - M3
+Black Wire of Back Motor - M4
+
+Red Wire of Front Motor - M1
+Black Wire of Back Motor - M2
+*/
+
