776995cbbf2baf3e69224d629f248992d503fa3b
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-04-30 00:29:44 +0530

    Created a separate function to calculate difference in coordinates between points

--- Gesture Controlled Vehicle/Gesture-Car-Control.py
+++ Gesture Controlled Vehicle/Gesture-Car-Control.py
@@ -55,10 +55,15 @@ def getCoordinatesofPoints(hand_landmarks,start,stop,step):
     return {"x": np.multiply(np.array([points.x for points in hand_landmarks.landmark[start:stop:step]]),frame_shape["width"]),
             "y": np.multiply(np.array([points.y for points in hand_landmarks.landmark[start:stop:step]]),frame_shape["height"]) }
 
+def diffCoordinates(landmarks,range1,range2):
+    return getCoordinatesofPoints(landmarks,range1[0],
+                                  range1[1],range1[2])['y']-getCoordinatesofPoints(landmarks,range2[0],
+                                                                                   range2[1],range2[2])['y']
+
 def motionControl(left_hand_landmarks):
     global ESP8266_URL
-    tip_dip_diff_coord_y = getCoordinatesofPoints(left_hand_landmarks,8,24,4)["y"] - getCoordinatesofPoints(left_hand_landmarks,7,23,4)["y"]
-    tip_mcp_diff_coord_y = getCoordinatesofPoints(left_hand_landmarks,8,24,4)["y"] - getCoordinatesofPoints(left_hand_landmarks,5,21,4)["y"]
+    tip_dip_diff_coord_y = diffCoordinates(left_hand_landmarks,[8,24,4],[7,23,4])
+    tip_mcp_diff_coord_y = diffCoordinates(left_hand_landmarks,[8,24,4],[5,21,4])
 
     if np.all(tip_dip_diff_coord_y>0):
         # print("Move Forward")
