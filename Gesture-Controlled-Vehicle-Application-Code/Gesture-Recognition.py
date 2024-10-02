6914aa22b410c45289f6c31d41e6f08670b16901
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-10-02 11:46:47 +0530

    Initialized Class Connection and its Class Variables

--- Gesture-Controlled-Vehicle-Application-Code/Gesture-Recognition.py
+++ Gesture-Controlled-Vehicle-Application-Code/Gesture-Recognition.py
@@ -1,2 +1,14 @@
 
 #Creation of class Connection to connect to the μC
+class Connection:
+    def __init__(self,ESP8266_mDNS_URL,port):
+        '''
+        Data Members:
+        mDNS - Stores the mDNS URL of the μC to connect to it.
+        IP   - Stores the IP address of the μC obtained after the first GET request to the mDNS.
+        URL  - String to create the URL of the μC with its IP address 
+        '''
+        self.mDNS = ESP8266_mDNS_URL
+        self.IP = None
+        self.URL = "http://{}:"+f"{port}"
+    
