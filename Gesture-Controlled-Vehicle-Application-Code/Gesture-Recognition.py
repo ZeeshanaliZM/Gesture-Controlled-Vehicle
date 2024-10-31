bcf0086cfb071bf6e4c2724a7bad20afb7d8f222
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-10-31 22:16:29 +0530

    Modified cv2 import statement

--- Gesture-Controlled-Vehicle-Application-Code/Gesture-Recognition.py
+++ Gesture-Controlled-Vehicle-Application-Code/Gesture-Recognition.py
@@ -1,5 +1,5 @@
 from requests import get, exceptions
-from cv2 import VideoCapture, flip, waitKey, destroyAllWindows
+import cv2
 from collections import namedtuple
 
 FrameShape = namedtuple('Frame Shape',['width','height'])
