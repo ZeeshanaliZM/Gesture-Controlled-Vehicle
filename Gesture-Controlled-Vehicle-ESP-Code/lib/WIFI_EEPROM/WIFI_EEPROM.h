ef7718b58a238d94c1017e4570f9f56e016cea46
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-02 19:56:16 +0530

    Created WIFI_EEPROM.h header file and WIFI_EEPROM.cpp source file
    1. Contains definition of structure to store Wifi details.
    2.  Contains declaration of function storeWifiDetails to store the wifi details in the EEPROM.
    3. Contains declaration of function getWifiDetails to get the wifi details from the EEPROM.
    4. Contains the function definitions to get and store the Wifi Details to and from the EEPROM.

---
+++ Gesture-Controlled-Vehicle-ESP-Code/lib/WIFI_EEPROM/WIFI_EEPROM.h
@@ -0,0 +1,17 @@
+#ifndef WIFI_EEPROM_H
+#define WIFI_EEPROM_H
+
+//Structure to store the Wifi Details
+typedef struct WifiDetails{
+    char *SSID;
+    char *password;
+}WifiDetails;
+
+//Function storeWifiDetails stores the wifi details in the EEPROM and can be extracted during runtime
+void storeWifiDetails(char ssid[],char password[],int address);
+
+//Function getWifiDetails gets the Wifi details from the EEPROM
+WifiDetails getWifiDetails(int address);
+
+#endif
+
