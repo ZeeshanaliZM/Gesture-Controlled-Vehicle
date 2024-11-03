063462ee44d6eeaa5384b1a2249f4bad0985d557
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-03 12:02:54 +0530

    Created Server_Connect.h header file and its source code Server_Connect.c to connect and setup the server
    1. Contains the macro definitions of the paths to issue commands to the webserver
    2. Contains the declaration of the functions to connect to the network, setup mDNS and the webserver on the ESP8266.

---
+++ Gesture-Controlled-Vehicle-ESP-Code/lib/Server_Connect/Server_Connect.h
@@ -0,0 +1,44 @@
+#ifndef SERVER_CONNECT_H
+#define SERVER_CONNECT_H
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "Control_Car.h"
+#include "WIFI_EEPROM.h"
+
+//Define Macro for the Handlers
+#define ROOT "/"
+#define FORWARD "/moveForward"
+#define BACKWARD "/moveBackward"
+#define DIRECTION "/directionControl"
+#define SPEED "/speedControl"
+
+//Connect to the Wifi. Retrieve the Wifi Credentials from the EEPROM
+void connecttoWifi(int Address);
+
+//Root Handler for webserver
+void rootHandle();
+
+//Setup mDNS to the webserver to get the IP address of the vehicle
+void setupmDNS();
+
+//Handler to move the car Forward when /moveForward Route is reached
+void forwardHandler();
+
+//Handler to move the car backward when /moveBackward Route is reached
+void backwardHandler();
+
+//Handler to change the direction of the car when /directionControl Route is reached
+void directionControlHandler();
+
+//Handler to change the speed of the car when /speedControl Route is reached
+void speedControlHandler();
+
+//Link Endpoints and Handlers together
+void URLHandler();
+
+//Object to create and modify webserver on ESP8266
+ESP8266WebServer server(80);
+
+#endif
