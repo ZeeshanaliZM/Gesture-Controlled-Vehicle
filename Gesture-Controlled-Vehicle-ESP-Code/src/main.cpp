31f4c30814680b7601b52ecc3ad64e2dae5afa3c
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-03 12:10:40 +0530

    Created the source code for the main file uploaded to the microcontroller.
    1. Store the address in the EEPROM of the microcontroller if not stored.
    2. Connect to the Wifi. Get the credentials from the EEPROM.
    3. Setup mDNS on the server.
    4. Start the server on port 80.
    5. Setup the URL endpoints of the server and configure the pins of the vehicle.

--- Gesture-Controlled-Vehicle-ESP-Code/src/main.cpp
+++ Gesture-Controlled-Vehicle-ESP-Code/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include "Server_Connect.h"
 
-// put function declarations here:
-int myFunction(int, int);
+//Define the EEPROM address to read the Wifi Credentials from
+#define EEPROM_ADDRESS int(0)
+
+//Store the wifi credentials in EEPROM if not stored. Delete these after they are stored in the EEPROM
+// const char* ssid PROGMEM = "Enter the wifi Name";
+// const char* password PROGMEM = "Enter the Wifi Password";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  // // Store Wifi Details in EEPROM (Run only once to Store the address). Uncomment to store credentials in EEPROM
+  // storeWifiDetails(FPSTR(ssid),FPSTR(password),EEPROM_ADDRESS);
+  
+  Serial.begin(115200);
+  
+  //Connect the the Wifi
+  connecttoWifi(EEPROM_ADDRESS);
+
+  //Setup mDNS on the server
+  setupmDNS();
+  
+  //Start the server
+  server.begin();
+
+  //Configure the server to listen on port 80
+  MDNS.addService("http", "tcp", 80);
+
+  //Configure the URLs of the server
+  URLHandler(); 
+
+  //Configure the pins of the vehicle
+  configMotorPins();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //Update mDNS
+  MDNS.update();
+
+  //Configure server to listen and handle requests from client
+  server.handleClient();
+  
 }
