05ffe03958976a262259514fcf3f0a23f6177828
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-05 23:40:52 +0530

    Modified WIFI_EEPROM.cpp
    1. Added EEPROM.begin() calls which is required to use the flash memory as an emulated eeprom. Pass the memory size to be used as argument to the call.
    2. Add EEPROM.end() statement to the functions.

--- Gesture-Controlled-Vehicle-ESP-Code/lib/WIFI_EEPROM/WIFI_EEPROM.cpp
+++ Gesture-Controlled-Vehicle-ESP-Code/lib/WIFI_EEPROM/WIFI_EEPROM.cpp
@@ -5,12 +5,16 @@ void storeWifiDetails(char ssid[30],char password[10],int address){
     WifiDetails Wifi;
     strcpy(Wifi.SSID,ssid);
     strcpy(Wifi.password,password);
+    EEPROM.begin(256);
     EEPROM.put(address,Wifi);
+    EEPROM.end();
 }
 
 //Function to get the wifi details from the EEPROM
 WifiDetails getWifiDetails(int address){
     WifiDetails Wifi;
+    EEPROM.begin(256);
     EEPROM.get(address,Wifi);
+    EEPROM.end();
     return Wifi;
 }
