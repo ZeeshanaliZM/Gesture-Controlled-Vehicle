a210c6017bd25b3398870b9fd9391acca69d6da4
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-05 23:52:19 +0530

    Modified main.cpp
    1. Replace pointers to array of character to character of specific lengths.
    2. Added Serial.begin() to begin communication with serial monitor.

--- Gesture-Controlled-Vehicle-ESP-Code/src/main.cpp
+++ Gesture-Controlled-Vehicle-ESP-Code/src/main.cpp
@@ -4,18 +4,16 @@
 //Define the EEPROM address to read the Wifi Credentials from
 #define EEPROM_ADDRESS int(0)
 
-//Store the wifi credentials in EEPROM if not stored. Delete these after they are stored in the EEPROM
-// const char* ssid PROGMEM = "Enter the wifi Name";
-// const char* password PROGMEM = "Enter the Wifi Password";
+// //Store the wifi credentials in EEPROM if not stored. Delete these after they are stored in the EEPROM
+// char ssid[30] = "Enter Wifi SSID";
+// char password[10] = "Enter Password";
 
 void setup() {
-
+  Serial.begin(9600);
   // // Store Wifi Details in EEPROM (Run only once to Store the address). Uncomment to store credentials in EEPROM
-  // storeWifiDetails(FPSTR(ssid),FPSTR(password),EEPROM_ADDRESS);
-  
-  Serial.begin(115200);
-  
-  //Connect the the Wifi
+  // storeWifiDetails(ssid,password,EEPROM_ADDRESS);
+
+  // //Connect the the Wifi
   connecttoWifi(EEPROM_ADDRESS);
 
   //Setup mDNS on the server
