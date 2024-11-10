23d2687963ecf69ead7ebf99159edfafc56c269e
Zeeshanali Merchant <zeeshanalimerchant22@gmail.com>
2024-11-10 21:15:15 +0530

    Modified Pin_Definitions.h
    1. Changed definitions of pins since Servo motor is being used instead of dc motor for direction control.
    2. Only servo motor can be used for precise direction control.

--- Gesture-Controlled-Vehicle-ESP-Code/lib/Pin_Definitions/Pin_Definitions.h
+++ Gesture-Controlled-Vehicle-ESP-Code/lib/Pin_Definitions/Pin_Definitions.h
@@ -2,8 +2,7 @@
 Red Wire of Back Motor - M3
 Black Wire of Back Motor - M4
 
-Red Wire of Front Motor - M1
-Black Wire of Back Motor - M2
+Servo Motor used for Front wheel axis for precision direction control
 */
 
 #ifndef PIN_DEFINITIONS_H
@@ -13,8 +12,6 @@ Black Wire of Back Motor - M2
 #define DRIVER_IN3_BACK D1
 #define DRIVER_EN2_BACK_SPEED D2
 
-#define DRIVER_IN2_FRONT D5
-#define DRIVER_IN1_FRONT D6
-#define DRIVER_EN1_FRONT_DIRECTION D7
+#define FRONT_WHEELS D7
 
 #endif
